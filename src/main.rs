1b017781eaae40542ff01fe9d6055d3f18c0f34a
Jan Starke <jan.starke@outofbed.org>
2021-12-14 22:31:20 +0100

    flag for displaying values

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use std::fs::File;
 struct RegLsApp {
     reg_file: File,
     print_recursive: bool,
+    print_values: bool,
+    path: Option<Vec<String>>
 }
 
 impl RegLsApp {
@@ -16,19 +18,70 @@ impl RegLsApp {
     pub fn new(file: File) -> Self {
         Self {
             reg_file: file,
-            print_recursive: false
+            print_recursive: false,
+            print_values: false,
+            path: None
         }
     }
+
     pub fn with_recursive(mut self, recursive: bool) -> Self {
         self.print_recursive = recursive;
         self
     }
+
+    pub fn with_print_values(mut self, values: bool) -> Self {
+        self.print_values = values;
+        self
+    }
+
+    pub fn with_path(mut self, path: Vec<String>) -> Self {
+        self.path = Some(path);
+        self
+    }
+
     pub fn run(&mut self) -> Result<()> {
         let mut hive = Hive::from_source(&self.reg_file)?;
         let mut root = hive.get_root_node()?;
-        self.print_node(root, 0)?;
+
+        if let Some(path) = self.path.clone() {
+            root = match self.find_node(root, &path)? {
+                None => { return Ok(())},
+                Some(n) => n
+            }
+        }
+
+        if self.print_values {
+            self.print_node_values(root, 0)?;
+        } else {
+            self.print_node(root, 0)?;
+        }
         Ok(())
     }
+
+    fn find_node(&mut self, mut root: NodeKey, path: &Vec<String>) -> Result<Option<NodeKey>> {
+        for entry in path {
+            let mut found_entry = false;
+
+            loop {
+                let record = match root.get_next_key(&mut self.reg_file)? {
+                    None => {break;}
+                    Some(node) => node
+                };
+
+                if record.key_name() == entry {
+                    root = record;
+                    found_entry = true;
+                    break;
+                }
+            }
+
+            if ! found_entry {
+                return Ok(None);
+            }
+        }
+        Ok(Some(root))
+    }
+
     fn print_node(&mut self, mut node: NodeKey, level: usize) -> Result<()> {
         let indent = "  ".repeat(level);
         loop {
@@ -36,6 +89,7 @@ impl RegLsApp {
                 None => {break;}
                 Some(node) => node
             };
+
             println!("{}{}", indent, record.key_name());
 
             if self.print_recursive {
@@ -44,6 +98,21 @@ impl RegLsApp {
         }
         Ok(())
     }
+
+    fn print_node_values(&mut self, mut node: NodeKey, level: usize) -> Result<()> {
+        let indent = "  ".repeat(level);
+        loop {
+            let value = match node.get_next_value(&mut self.reg_file)? {
+                None => {break;}
+                Some(node) => node
+            };
+
+            println!("{}{} = ", indent, value.get_name());
+
+        }
+        Ok(())
+    }
+
     pub fn parse_options() -> Result<RegLsApp> {
         let app = App::new(env!("CARGO_PKG_NAME"))
             .version(env!("CARGO_PKG_VERSION"))
@@ -57,12 +126,27 @@ impl RegLsApp {
                     .takes_value(true),
             )
             .arg(
+                Arg::with_name("PATH")
+                    .help("registry path")
+                    .required(false)
+                    .multiple(false)
+                    .takes_value(true),
+            )
+            .arg(
                 Arg::with_name("RECURSIVE")
                     .help("print recursively")
                     .multiple(false)
                     .takes_value(false)
                     .short("R")
                     .long("recursive")
+            )
+            .arg(
+                Arg::with_name("VALUES")
+                    .help("print only values")
+                    .multiple(false)
+                    .takes_value(false)
+                    .short("V")
+                    .long("values")
             )
         ;
         let matches = app.get_matches();
@@ -77,9 +161,15 @@ impl RegLsApp {
             File::open(fp)?
         };
 
-        Ok(RegLsApp::new(reg_file)
+        let mut app = RegLsApp::new(reg_file)
             .with_recursive(matches.is_present("RECURSIVE"))
-        )
+            .with_print_values(matches.is_present("VALUES"));
+
+        if let Some(path) = matches.value_of("PATH") {
+            let parts: Vec<String> = path.split("/").map(|x|x.to_owned()).collect();
+            app = app.with_path(parts);
+        }
+        Ok(app)
     }
 }
 
